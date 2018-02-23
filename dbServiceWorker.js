f60c5f83f84d7fdb5a8ead553c7e2b36b6050400
matt <vaporlinx@gmail.com>
2018-02-22 20:05:10 -0700

    use indexDb to store units

--- dbServiceWorker.js
+++ dbServiceWorker.js
@@ -1,5 +1,6 @@
 // TODO: turn unit cache into an indexDB (ewwwwww)
-const unitCache = {};
+let unitDB = null;
+const currentRequests = {};
 const unitTypes = ["BM", "IM", "PM", "CV", "SV", "AF", "CF", "DS", "DA", "SC", "MS", "CI", "BA"];
 const validSearchParams = [
   "name",
@@ -9,40 +10,82 @@ const validSearchParams = [
 ];
 const unitPreCache = ["atlas", "awesome", "catapult"];
 
-const getNested = (source, props) => {
-  let property = source;
-  for (const prop of props) {
-    if (!property === undefined && !property[prop] === undefined) {
-      property = property[prop];
+// const getNested = (source, props) => {
+//   let property = source;
+//   for (const prop of props) {
+//     if (!property === undefined && !property[prop] === undefined) {
+//       property = property[prop];
+//     }
+//     else {
+//       property = undefined;
+//       break;
+//     }
+//   }
+//   return property;
+// };
+
+const dbConnection = indexedDB.open("unitDB", 1);
+dbConnection.onerror = event => {
+  console.log("error opening db");
+};
+
+dbConnection.onupgradeneeded = event => {
+  for (const unitType of unitTypes) {
+    event.target.result.createObjectStore(unitType, {keyPath: "name"});
+  }
+};
+
+const getUnit = (type, name) => {
+  return new Promise((resolve, reject) => {
+    if (unitDB) {
+      const request = unitDB.transaction(type).objectStore(type).get(name);
+      request.onerror = event => {
+        reject(`Failed to get read transaction for type: ${type}, name: ${name}`);
+      };
+      request.onsuccess = event => {
+        resolve(request.result);
+      };
     }
     else {
-      property = undefined;
-      break;
+      reject("Unit database not initialized!");
     }
-  }
-  return property;
+  });
 };
 
-// TODO: it looks like there is a bug with this where it overwrites the base object
-const setNested = (source, props, value) => {
-  console.log(props);
-  let property = source;
-  for (let i = 0; i < props.length - 1; i++) {
-    if (!property[props[i]]) {
-      property[props[i]] = {};
+const getUnits = type => {
+  return new Promise((resolve, reject) => {
+    if (unitDB) {
+      const request = unitDB.transaction(type).objectStore(type).getAll();
+      request.onerror = event => {
+        reject(`Failed to get read transaction for type: ${type}`);
+      };
+      request.onsuccess = event => {
+        resolve(request.result);
+      };
     }
-    property = property[props[i]];
-  }
-  property[props[props.length - 1]] = value;
-
+    else {
+      reject("Unit database not initialized!");
+    }
+  });
 };
 
-const spreadObject = source => {
-  const spread = [];
-  for (const key of Object.keys(source)) {
-    spread.push(source[key]);
-  }
-  return spread;
+const setUnit = (type,  data) => {
+  return new Promise((resolve, reject) => {
+    if (unitDB) {
+      try {
+        const request = unitDB.transaction(type, "readwrite").objectStore(type).put(data);
+        request.onsuccess = event => {
+          resolve();
+        };
+      }
+      catch (err) {
+        reject(`setUnit failed for: ${type}, name: ${data.name}`);
+      }
+    }
+    else {
+      reject("Unit database not initialized!");
+    }
+  });
 };
 
 const serveOrFetch = request => {
@@ -72,59 +115,68 @@ const searchUnits = url => {
     const results = [];
     let unitsSearched = 0;
     for (const type of searchParams.types || unitTypes) {
-      const unitsByType = unitCache[type] || {};
-      for (const unitClasses of spreadObject(unitsByType)) {
-        for (const unit of spreadObject(unitClasses)) {
-          unitsSearched++;
-          let valid = true;
-          if (searchParams.name && !unit.name.toLowerCase().includes(searchParams.name)) {
-            valid = false;
-          }
-          if (searchParams.minPV && unit.pv <= searchParams.minPV) {
-            valid = false;
-          }
-          if (searchParams.maxPV && unit.pv >= searchParams.maxPV) {
-            valid = false;
-          }
-          if (valid) {
-            results.push(unit);
-          }
+      const units = await getUnits(type);
+      for (const unit of units) {
+        unitsSearched++;
+        let valid = true;
+        if (searchParams.name && !unit.name.toLowerCase().includes(searchParams.name)) {
+          valid = false;
+        }
+        if (searchParams.minPV && unit.pv <= searchParams.minPV) {
+          valid = false;
+        }
+        if (searchParams.maxPV && unit.pv >= searchParams.maxPV) {
+          valid = false;
+        }
+        if (valid) {
+          results.push(unit);
         }
       }
     }
     console.log(`UnitsSearched: ${unitsSearched}`);
     const response = new Response(JSON.stringify(results));
     resolve(response);
-       // TODO: reduce searchParams into a string of query params
-      // const queryString = Object.keys(searchParams).reduce();
-      const unParsed = await fetch(`http://masterunitlist.info/Unit/QuickList${url.search}`);
-      const data = JSON.parse(await unParsed.text()).Units;
-      for (const datum of data) {
-        const unit = {
-          name: datum.Name,
-          pv: datum.BFPointValue,
-          armor: datum.BFArmor,
-          structure: datum.BFStructure,
-          damage: {
-            short: datum.BFDamageShort,
-            medium: datum.BFDamageMedium,
-            long: datum.BFDamageLong,
-          },
-          movement: datum.BFMove,
-          image: datum.ImageUrl,
-          type: datum.BFType,
-          size: datum.BFSize,
-          role: datum.Role.Name,
-          skill: datum.Skill || 4,
-          special: datum.BFAbilities,
-          class: datum.Class,
-          variant: datum.Variant,
-        };
-        if (!getNested(unitCache, [unit.type, unit.class, unit.variant])) {
-          setNested(unitCache, [unit.type, unit.class, unit.variant], unit);
+      const queryString = Object.keys(searchParams).reduce((queryString, key) => {
+        return `${queryString}${key}=${searchParams[key]}`;
+      }, "?");
+      currentRequests[url] = fetch(`http://masterunitlist.info/Unit/QuickList${queryString}`)
+        .then(request => request.text()).then(unParsed => JSON.parse(unParsed).Units).then(data => {
+        for (const datum of data) {
+          const unit = {
+            name: datum.Name,
+            pv: datum.BFPointValue,
+            armor: datum.BFArmor,
+            structure: datum.BFStructure,
+            damage: {
+              short: datum.BFDamageShort,
+              medium: datum.BFDamageMedium,
+              long: datum.BFDamageLong,
+            },
+            movement: datum.BFMove,
+            image: datum.ImageUrl,
+            type: datum.BFType,
+            size: datum.BFSize,
+            role: datum.Role.Name,
+            skill: datum.Skill || 4,
+            special: datum.BFAbilities,
+            class: datum.Class,
+            variant: datum.Variant,
+          };
+          // Base omnimechs have no type for some reason
+          if (unit.type !== null) {
+            setUnit(unit.type, unit);
+          }
         }
-      }
+      });
   });
+};
+
+dbConnection.onsuccess = async event => {
+  unitDB = event.target.result;
+  for (const unit of unitPreCache) {
+    const url = new URL(`http://internaldb/?name=${unit}`);
+    await searchUnits(url);
+  }
 };
 
 self.addEventListener("fetch", event => {
@@ -137,12 +189,5 @@ self.addEventListener("fetch", event => {
   }
   else if (url.hostname === "internaldb") {
     event.respondWith(searchUnits(url));
-  }
-});
-
-self.addEventListener("install", async event => {
-  for (const unit of unitPreCache) {
-    const url = new URL(`http://internaldb/?name=${unit}`);
-    await searchUnits(url);
   }
 });
