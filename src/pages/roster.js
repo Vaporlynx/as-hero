084e12f85197d2e49a2542ae1330ea1a33ea7b85
matt <vaporlinx@gmail.com>
2018-03-07 19:41:22 -0700

    clean up roster ui

--- src/pages/roster.js
+++ src/pages/roster.js
@@ -3,6 +3,17 @@ import * as urlHelper from "../../src/urlHelper.js";
 const template = document.createElement("template");
 template.innerHTML = `
     <style>
+        #roster {
+            display: flex;
+            width: 100vw;
+            height: 100vh;
+            overflow: auto;
+            flex-wrap: wrap;
+        }
+
+        #roster > unit-card {
+            margin: 5px;
+        }
     </style>
 
     <div id="roster"> </div>
