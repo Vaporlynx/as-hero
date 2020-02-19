20b27eaecc03a1b70b197f1a9b56a1aa598741d3
f <f>
2020-02-18 22:55:30 -0700

    tweak db service worker error handling

--- dbServiceWorker.js
+++ dbServiceWorker.js
@@ -138,88 +138,94 @@ const searchUnits = url => {
   return new Promise(async (resolve, reject) => {
     let unitsSearched = 0;
     let results = [];
-
+    
     for (const type of searchParams.types || unitTypes) {
-      const units = await getUnits(type);
-      for (const unit of units) {
-        unitsSearched++;
-        // TODO: these if statements are very similar.  Look at programatic way to organize them, maintain maintainability
-        let valid = true;
-        const parsedMove = unit.movement.replace(/[\\"a-z]/ig, "").split("/").map(i => parseInt(i));
-        const parsedDamage = [unit.damage.short, unit.damage.medium, unit.damage.long];
-        let targetMovementModifier = 0;
-        if (parsedMove[0] < 5) {
-          targetMovementModifier = 0;
-        }
-        else if (parsedMove[0] < 9) {
-            targetMovementModifier = 1;
-        }
-        else if (parsedMove[0] < 13) {
-            targetMovementModifier = 2;
-        }
-        else if (parsedMove[0] < 19) {
-            targetMovementModifier = 3;
-        }
-        else if (parsedMove[0] < 35) {
-            targetMovementModifier = 4;
-        }
-        else if (parsedMove[0] >= 35) {
-            targetMovementModifier = 5;
-        }
+      try {
+        const units = await getUnits(type);
+        for (const unit of units) {
+          unitsSearched++;
+          // TODO: these if statements are very similar.  Look at programatic way to organize them, maintain maintainability
+          let valid = true;
+          const parsedMove = unit.movement.replace(/[\\"a-z]/ig, "").split("/").map(i => parseInt(i));
+          const parsedDamage = [unit.damage.short, unit.damage.medium, unit.damage.long];
+          let targetMovementModifier = 0;
+          if (parsedMove[0] < 5) {
+            targetMovementModifier = 0;
+          }
+          else if (parsedMove[0] < 9) {
+              targetMovementModifier = 1;
+          }
+          else if (parsedMove[0] < 13) {
+              targetMovementModifier = 2;
+          }
+          else if (parsedMove[0] < 19) {
+              targetMovementModifier = 3;
+          }
+          else if (parsedMove[0] < 35) {
+              targetMovementModifier = 4;
+          }
+          else if (parsedMove[0] >= 35) {
+              targetMovementModifier = 5;
+          }
 
-        if (valid && searchParams.unitIds && searchParams.unitIds.length && !searchParams.unitIds.includes(unit.id)) {
-          valid = false;
-        }
-        if (valid && searchParams.unitName && !unit.name.toLowerCase().includes(searchParams.unitName.toLowerCase())) {
-          valid = false;
-        }
-        if (valid && searchParams.minPV && unit.pv < parseInt(searchParams.minPV)) {
-          valid = false;
-        }
-        if (valid && searchParams.maxPV && unit.pv > parseInt(searchParams.maxPV)) {
-          valid = false;
-        }
-        if (valid && searchParams.minSpeed && parsedMove.every(i => i < parseInt(searchParams.minSpeed))) {
-          valid = false;
-        }
-        if (valid && searchParams.maxSpeed && parsedMove.every(i => i > parseInt(searchParams.maxSpeed))) {
-          valid = false;
-        }
-        if (valid && searchParams.minDamage && parsedDamage.every(i => i < parseInt(searchParams.minDamage))) {
-          valid = false;
-        }
-        if (valid && searchParams.maxDamage && parsedDamage.every(i => i > parseInt(searchParams.maxDamage))) {
-          valid = false;
-        }
-        if (valid && searchParams.minTMM && targetMovementModifier < parseInt(searchParams.minTMM)) {
-          valid = false;
-        }
-        if (valid && searchParams.maxTMM && targetMovementModifier > parseInt(searchParams.maxTMM)) {
-          valid = false;
-        }
-        if (valid && searchParams.minPD && unit.metadata.productionDate < parseInt(searchParams.minPD)) {
-          valid = false;
-        }
-        if (valid && searchParams.maxPD && unit.metadata.productionDate > parseInt(searchParams.maxPD)) {
-          valid = false;
-        }
-        if (valid && searchParams.techLevels && searchParams.techLevels.length && !searchParams.techLevels.includes(unit.metadata.techLevel)) {
-          valid = false;
-        }
-        if (valid && searchParams.sizes && searchParams.sizes.length && !searchParams.sizes.includes(unit.size)) {
-          valid = false;
-        }
-        if (valid && searchParams.specials && searchParams.specials.length) {
-          for (const special of searchParams.specials) {
-            if (!unit.special || !unit.special.toLowerCase().includes(special)) {
-              valid = false;
-              break;
+          if (valid && searchParams.unitIds && searchParams.unitIds.length && !searchParams.unitIds.includes(unit.id)) {
+            valid = false;
+          }
+          if (valid && searchParams.unitName && !unit.name.toLowerCase().includes(searchParams.unitName.toLowerCase())) {
+            valid = false;
+          }
+          if (valid && searchParams.minPV && unit.pv < parseInt(searchParams.minPV)) {
+            valid = false;
+          }
+          if (valid && searchParams.maxPV && unit.pv > parseInt(searchParams.maxPV)) {
+            valid = false;
+          }
+          if (valid && searchParams.minSpeed && parsedMove.every(i => i < parseInt(searchParams.minSpeed))) {
+            valid = false;
+          }
+          if (valid && searchParams.maxSpeed && parsedMove.every(i => i > parseInt(searchParams.maxSpeed))) {
+            valid = false;
+          }
+          if (valid && searchParams.minDamage && parsedDamage.every(i => i < parseInt(searchParams.minDamage))) {
+            valid = false;
+          }
+          if (valid && searchParams.maxDamage && parsedDamage.every(i => i > parseInt(searchParams.maxDamage))) {
+            valid = false;
+          }
+          if (valid && searchParams.minTMM && targetMovementModifier < parseInt(searchParams.minTMM)) {
+            valid = false;
+          }
+          if (valid && searchParams.maxTMM && targetMovementModifier > parseInt(searchParams.maxTMM)) {
+            valid = false;
+          }
+          if (valid && searchParams.minPD && unit.metadata.productionDate < parseInt(searchParams.minPD)) {
+            valid = false;
+          }
+          if (valid && searchParams.maxPD && unit.metadata.productionDate > parseInt(searchParams.maxPD)) {
+            valid = false;
+          }
+          if (valid && searchParams.techLevels && searchParams.techLevels.length && !searchParams.techLevels.includes(unit.metadata.techLevel)) {
+            valid = false;
+          }
+          if (valid && searchParams.sizes && searchParams.sizes.length && !searchParams.sizes.includes(unit.size)) {
+            valid = false;
+          }
+          if (valid && searchParams.specials && searchParams.specials.length) {
+            for (const special of searchParams.specials) {
+              if (!unit.special || !unit.special.toLowerCase().includes(special)) {
+                valid = false;
+                break;
+              }
             }
           }
+          if (valid) {
+            results.push(Object.assign(unit, {totalArmor: unit.armor, totalStructure: unit.structure}));
+          }
         }
-        if (valid) {
-          results.push(Object.assign(unit, {totalArmor: unit.armor, totalStructure: unit.structure}));
-        }
+
+      }
+      catch (err) {
+        // Swallow error
       }
     }
     if (searchParams.unitIds && searchParams.unitIds.length) {
