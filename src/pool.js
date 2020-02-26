05e228b4a81c090f4dc7d74660c4d4e2fbadeff1
matt <vaporlinx@gmail.com>
2020-02-25 19:45:39 -0700

    add simple pooling util

---
+++ src/pool.js
@@ -0,0 +1,36 @@
+const pools = new Map();
+
+const getPool = (symbol) => {
+    if (!pools.has(symbol)) {
+        throw "POOL_NOT_DEFINED";
+    }
+
+    return pools.get(symbol);
+}
+
+export const initializePool = (symbol, constructor, maxCacheCount) => {
+    if (pools.has(symbol)) {
+        throw "POOL_SYMBOL_COLLISION";
+    }
+
+    pools.set(symbol, {constructor, maxCacheCount, instances: []});
+}
+
+export const getFromPool = (symbol) => {
+    const pool = getPool(symbol);
+
+    if (pool.instances.length) {
+        return pool.instances.shift();
+    }
+    else {
+        return pool.constructor();
+    }
+}
+
+export const returnToPool = (symbol, instance) => {
+    const pool = getPool(symbol);
+
+    if (pool.instances.length < pool.maxCacheCount) {
+        pool.instances.push(instance);
+    }
+}
