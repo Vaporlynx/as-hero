7632b1536a0f7654aaf1fb0ad9e870e5cee42abb
matt <vaporlinx@gmail.com>
2020-02-25 19:58:48 -0700

    fix lint issues, pass through constructor arguments

--- src/pool.js
+++ src/pool.js
@@ -1,12 +1,12 @@
 const pools = new Map();
 
-const getPool = (symbol) => {
+const getPool = symbol => {
     if (!pools.has(symbol)) {
         throw "POOL_NOT_DEFINED";
     }
 
     return pools.get(symbol);
-}
+};
 
 export const initializePool = (symbol, constructor, maxCacheCount) => {
     if (pools.has(symbol)) {
@@ -16,16 +16,16 @@ export const initializePool = (symbol, constructor, maxCacheCount) => {
     pools.set(symbol, {constructor, maxCacheCount, instances: []});
 }
 
-export const getFromPool = (symbol) => {
+export const getFromPool = (symbol, constructorArguments) => {
     const pool = getPool(symbol);
 
     if (pool.instances.length) {
         return pool.instances.shift();
     }
     else {
-        return pool.constructor();
+        return pool.constructor(constructorArguments);
     }
-}
+};
 
 export const returnToPool = (symbol, instance) => {
     const pool = getPool(symbol);
@@ -33,4 +33,4 @@ export const returnToPool = (symbol, instance) => {
     if (pool.instances.length < pool.maxCacheCount) {
         pool.instances.push(instance);
     }
-}
+};
